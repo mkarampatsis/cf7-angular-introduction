cb2c78de8785ed3091803eb0978b605889494f4f
mkarampatsis <marka@central.ntua.gr>
2025-04-28 18:32:37 +0300

    angular navbar

--- src/app/components/component-input-example/component-input-example.component.ts
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-component-input-example',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './component-input-example.component.css'
 })
 export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'Christodoulos',
+    surName: 'Fragkoudakis',
+    age: 55,
+    email: 'chfrag@aueb',
+    address: 'New York, Usa'
+  }
 
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 45,
+    email: 'john@aueb.gr',
+    address: 'Athens Greece'
+  }
 }
