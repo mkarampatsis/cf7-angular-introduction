35862d83c6b5c9ca7eed107814e76fc750ae6b3c
mkarampatsis <marka@central.ntua.gr>
2025-04-28 18:56:51 +0300

    angular navbar

--- src/app/components/component-input-example/component-input-example.component.ts
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Person } from 'src/app/shared/interfaces/person';
+import { PersonTableComponent } from '../person-table/person-table.component';
 
 @Component({
   selector: 'app-component-input-example',
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './component-input-example.component.html',
   styleUrl: './component-input-example.component.css'
 })
