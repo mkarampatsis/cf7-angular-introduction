f5ce0cd78f7ab7f957dae38853e0b25ea40658ca
mkarampatsis <marka@central.ntua.gr>
2025-05-12 20:04:27 +0300

    user registration

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -10,6 +10,7 @@ import {
   Validators 
 } from '@angular/forms';
 import { UserService } from 'src/app/shared/services/user.service';
+import { User } from 'src/app/shared/interfaces/user';
 
 @Component({
   selector: 'app-user-registration',
@@ -55,14 +56,36 @@ export class UserRegistrationComponent {
   }
 
   onSubmit(){
-    const data = this.form.value;
+    // const data = this.form.value as User;
+    const data: User = {
+      'username': this.form.get('username')?.value || '',
+      'password': this.form.get('password')?.value || '',
+      'name': this.form.get('name')?.value || '',
+      'surname': this.form.get('surname')?.value || '',
+      'email':this.form.get('email')?.value || '',
+      'address': {
+        'area':this.form.get('area')?.value || '',
+        'road': this.form.get('road')?.value || ''
+      }
+    }
     console.log(data);
+    this.userService.registerUser(data)
+      .subscribe({
+        next: (response) => {
+          console.log("User Saved", response);
+        },
+        error: (response) => {
+          console.log("User not Saved", response)
+        }
+      })
+    
   }
 
   check_dublicate_email(){
     const email = this.form.get("email")?.value;
 
     if (email){
+      console.log("email", email);
       this.userService.check_dublicate_email(email)
         .subscribe({
           next: (response) => {
